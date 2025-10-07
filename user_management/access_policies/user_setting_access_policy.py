ab0fcbfc789ae05e3556bfed69bf84235d3a2a22
phupha-sbp888 <n.krittin@thegang.tech>
2025-10-07 17:25:17 +0700

    feat: add user API access policy and test cases for testing user API with permission

---
+++ user_management/access_policies/user_setting_access_policy.py
@@ -0,0 +1,37 @@
+"""Access policy statements of user CRUD API."""
+
+from typing import TYPE_CHECKING
+
+from rest_access_policy import AccessPolicy
+from rest_framework.request import Request
+
+if TYPE_CHECKING:
+    from user_management.views.user_viewset import UserViewSet
+
+
+class UserSettingAccessPolicy(AccessPolicy):
+    """Access policy defining user API permissions.
+
+    Permissions explanation:
+    1. Admin users can perform any operation on all other users.
+    2. Normal users such as client accounts can only view and update their own user information.
+    """
+
+    statements = [
+        {
+            "action": ["update", "partial_update", "retrieve"],
+            "principal": "*",
+            "effect": "allow",
+            "condition": "is_request_own_account",
+        },
+        {"action": ["*"], "principal": "*", "effect": "allow", "condition": "is_admin"},
+    ]
+
+    def is_admin(self, request: Request, view: 'UserViewSet', action: str) -> bool:
+        """Override default admin principal with custom admin field."""
+        return not request.user.is_anonymous and request.user.is_admin
+
+    def is_request_own_account(self, request: Request, view: 'UserViewSet', action: str) -> bool:
+        """Check if request user is fetching or updating his own information."""
+        query_user = view.get_object()
+        return request.user == query_user
--- user_management/tests/views/test_user_viewset.py
+++ user_management/tests/views/test_user_viewset.py
@@ -29,6 +29,10 @@ class TestUserViewSet(APITestCase):
             "last_name": "user",
         }
 
+    def setUp(self) -> None:
+        """Login with admin user."""
+        self.client.force_authenticate(user=self.admin_user)
+
     def _validate_response_data(self, response: Response, expected_data: Dict[str, Union[bool, str, int]]) -> None:
         """Validate nessesary fields in user response data."""
         self.assertEqual(response.data["username"], expected_data["username"])
@@ -50,43 +54,90 @@ class TestUserViewSet(APITestCase):
         self.assertEqual(response.data["count"], 2)
         self.assertEqual(response.data["results"], expected_result)
 
+    def test_list_users_with_normal_user(self) -> None:
+        """Test list all users with an account that has insufficient permission."""
+        url: str = reverse("users:list-users")
+        self.client.force_authenticate(user=self.normal_user)
+        response: Response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
     def test_retrieve_user(self) -> None:
-        """Test retrieving a specific user by ID."""
+        """Test retrieving a user information by owner."""
+        url: str = reverse("users:retrieve-user", args=[self.admin_user.user_id])
+        response: Response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['user_id'], self.admin_user.user_id)
+
+    def test_retrieve_other_user_info_with_admin_user(self) -> None:
+        """Test retrieving other user information with admin account."""
         url: str = reverse("users:retrieve-user", args=[self.normal_user.user_id])
         response: Response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['user_id'], self.normal_user.user_id)
 
-        # Test retriving non-existing user
+    def test_retrieve_other_user_info_with_normal_user(self) -> None:
+        """Test retrieving other user information with insufficient permission."""
+        url: str = reverse("users:retrieve-user", args=[self.admin_user.user_id])
+        self.client.force_authenticate(user=self.normal_user)
+        response: Response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_retrieve_non_existing_user(self) -> None:
+        """Test retrieving a non existing user ID."""
         url: str = reverse("users:retrieve-user", args=[self.normal_user.user_id + self.admin_user.user_id])
         response: Response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_create_user(self) -> None:
-        """Test creating a new user."""
+        """Test creating a new user with admin user."""
         url: str = reverse("users:create-user")
         response: Response = self.client.post(url, data=self.valid_user_create_input)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self._validate_response_data(response, self.valid_user_create_input)
 
+    def test_create_user_without_permission(self) -> None:
+        """Test creating a new user with normal account (without create permission)."""
+        url: str = reverse("users:create-user")
+        self.client.force_authenticate(user=self.normal_user)
+        response: Response = self.client.post(url, data=self.valid_user_create_input)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
     def test_update_user(self) -> None:
-        """Test updating a user information with a specific user ID."""
+        """Test updating a user information with a specific user ID and admin permission."""
         url: str = reverse("users:update-user", args=[self.normal_user.user_id])
         response: Response = self.client.put(url, data=self.valid_user_create_input)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self._validate_response_data(response, self.valid_user_create_input)
 
-    def test_partial_update_user(self) -> None:
-        """Test updating with some given information of a user with a specific user ID."""
+    def test_update_other_user_with_normal_user(self) -> None:
+        """Test updating other user information requested by normal user account."""
         url: str = reverse("users:update-user", args=[self.admin_user.user_id])
+        self.client.force_authenticate(user=self.normal_user)
+        response: Response = self.client.put(url, data=self.valid_user_create_input)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_partial_update_user(self) -> None:
+        """Test updating with some given information of a user with a specific user ID and admin permission."""
+        url: str = reverse("users:update-user", args=[self.normal_user.user_id])
         update_user_input: Dict[str, Union[bool, str, int]] = {
             "first_name": "update_first_name",
-            "last_name": "updae_last_name",
+            "last_name": "update_last_name",
         }
         response: Response = self.client.patch(url, data=update_user_input)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["first_name"], update_user_input["first_name"])
         self.assertEqual(response.data["last_name"], update_user_input["last_name"])
+
+    def test_partial_update_other_user_with_normal_user(self) -> None:
+        """Test updating with some given information of other users requested by normal user account."""
+        url: str = reverse("users:update-user", args=[self.admin_user.user_id])
+        update_user_input: Dict[str, Union[bool, str, int]] = {
+            "first_name": "update_first_name",
+            "last_name": "update_last_name",
+        }
+        self.client.force_authenticate(user=self.normal_user)
+        response: Response = self.client.patch(url, data=update_user_input)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_delete_user(self) -> None:
         """Test deleting the existing user."""
@@ -95,6 +146,15 @@ class TestUserViewSet(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertFalse(User.objects.filter(user_id=self.normal_user.user_id).exists())
 
-        # Test deleting non-existing user
+    def test_delete_non_existing_user(self) -> None:
+        """Test deleting user that does not exist."""
+        url: str = reverse("users:delete-user", args=[self.normal_user.user_id + 1])
         response: Response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_user_without_permission(self) -> None:
+        """Test deleting user requested by normal user account."""
+        url: str = reverse("users:delete-user", args=[self.normal_user.user_id])
+        self.client.force_authenticate(user=self.normal_user)
+        response: Response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
